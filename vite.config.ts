c78ad104a0481a6d369d3ae37a74d14ca5285b53
Jiri Peterek <jiri@peterek.net>
2024-01-30 12:03:59 +0100

    Review build process

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { basename, extname } from 'node:path'
+
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import pkg from './package.json' assert { type: 'json' }
+
+const defaultExport = pkg.exports['.'].default
+const entryName = basename(defaultExport, extname(defaultExport))
+
+export default defineConfig(({ mode }) => ({
+  build: {
+    lib: {
+      entry: {
+        [entryName]: 'src/index.ts'
+      },
+      formats: ['cjs', 'es']
+    },
+    rollupOptions: {
+      external: [
+        ...Object.keys(pkg.dependencies), // Use package.json dependencies as externals
+      ]
+    },
+    sourcemap: true,
+    minify: true
+  },
+  plugins: [tsconfigPaths(), dts()],
+}))
